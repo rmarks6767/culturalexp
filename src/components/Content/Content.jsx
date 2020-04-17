c6fbddbdcbf170ae3f3e3729da75f4805b40277f
River Marks <rmarks6767@gmail.com>
2020-04-16 22:00:41 -0400

    Added transitions between the different elements and a fade that occurs

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -11,50 +11,114 @@ class Content extends Component {
 
     constructor(props){
         super(props);
-        
-        // The data that will be displayed on the page
-        this.quotes = quotes;
-        this.images = [
-            abuGhraibg,
-            kosovarAlbanians,
-            napalmGirl,
-            simonSchamaPicasso,
-            twoBangladeshiBoys
-        ]
-        this.currentIndex = 0; //Math.floor(Math.random() * this.images.length);
+        this.state = {
+            elements: [],
+            visible: false,
+            currentIndex: Math.floor(Math.random() * 5)
+        }
+        this.changeImage();
     }
 
+    componentDidMount() {
+        // The time before each image will change
+        this.timerID = setInterval(
+          () => this.changeImage(),
+          15000
+        );
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    changeImage(){
+        this.setState({
+            visible: false,
+        });
+
+        var data = {
+            quotes: quotes,
+            images: [
+                abuGhraibg,
+                kosovarAlbanians,
+                napalmGirl,
+                simonSchamaPicasso,
+                twoBangladeshiBoys
+            ]
+        }
+
+        var nextIndex = (this.state.currentIndex + 1) % 5;
+
+        var picLocation = Math.floor(Math.random() * 6);
+        var newList = [];
+        var g = 0;
+
+        for(var i = 0; i < 6; i++){
+            var element = null;
+            if (i !== picLocation){
+                element = 
+                <div>
+                    <i>"{data.quotes[nextIndex][g].quote}"</i>
+                    <br></br>
+                    <br></br>
+                    <i>&mdash;&nbsp;{data.quotes[nextIndex][g].name}</i>
+                </div>
+                g++;
+            }
+            else {
+                element = 
+                <div> 
+                    <img src={data.images[nextIndex]} alt={data.images[nextIndex]}/>
+                </div>
+            }
+            newList.push(element);
+        }
+
+        setTimeout(() => {
+            this.setState({
+              visible: true,
+              currentIndex: nextIndex,
+              elements: newList
+            });
+          }, 2000);    
+        }
 
     render(){
         return (
-            <div className="tiles">
-                <div className="tile">
-                    <blockquote>"{this.quotes[this.currentIndex][0].quote}"</blockquote>
-                    <div className="">
-
+            <div className="stuff">
+                <div className="tiles">
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[0] }
+                        </div>
                     </div>
-                    {this.quotes[this.currentIndex][0].name}
-                </div>
-                <div class="tile">
-                    <blockquote>"{this.quotes[this.currentIndex][1].quote}"</blockquote>
-                    {this.quotes[this.currentIndex][1].name}
-                </div>
-                <div class="tile">
-                    <blockquote>"{this.quotes[this.currentIndex][2].quote}"</blockquote>
-                    {this.quotes[this.currentIndex][2].name}
-                </div>
-                <div class="tile">
-                    <img src={this.images[this.currentIndex]} alt={this.images[this.currentIndex]}/>
-                </div>
-                <div class="tile">
-                    <blockquote>"{this.quotes[this.currentIndex][3].quote}"</blockquote>
-                    {this.quotes[this.currentIndex][3].name}
-                </div>
-                <div class="tile">
-                    <blockquote>"{this.quotes[this.currentIndex][4].quote}"</blockquote>
-                    {this.quotes[this.currentIndex][4].name}
-                </div>
-            </div>  
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[1] }
+                        </div>
+                    </div>
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[2] }
+                        </div>                    
+                    </div>
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[3] }
+                        </div>                   
+                    </div>
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[4] }
+                        </div>                    
+                    </div>
+                    <div className="tile">
+                        <div className={ this.state.visible? 'tile-fadeIn':'tile-fadeOut'}> 
+                            { this.state.elements[5] }
+                        </div>                    
+                    </div>
+                </div>  
+            </div>
         );
     }
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import GithubLogo from './github.svg';
+import Card from 'react-bootstrap/Card'
 import './Footer.css';
 
 class Footer extends Component {
     render(){
         return (
-            <h5>River Marks &copy; 2020</h5>
-        );
+            <div className="Footer">
+                <div className="author">
+                    <p className="author-name">Site created by River Marks</p>
+                </div>
+                <div className="links">
+                    <a href="https://github.com/rmarks6767/culturalexp" className="github">
+                        <p className="githubLink">Check out this site out on Github!</p>
+                        <img id='github-logo' src={GithubLogo} alt='Github logo'/>
+                    </a>
+                </div>
+            </div>
+        )
     }
 }
 
